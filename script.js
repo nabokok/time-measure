7022e5293ae0398f10f602e833dd34ebded70d30
Alyona <alyonana42@gmail.com>
2022-12-16 15:47:25 +0200

    add coments

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const presetBtns = document.querySelectorAll('.preset');
 const countBtn = document.querySelector('.btn');
 const error = document.querySelector('.error');
 
+//виклик функції при завантаженні DOM, яка відобразить таблицю history
 document.addEventListener('DOMContentLoaded', () => {
     storeInHistory();
 });
@@ -28,6 +29,7 @@ presetBtns.forEach(btn => {
     clickBtnHandler(btn, presetBtns, 'presetType');
 });
 
+//при зміні значення інпута очищаємо помилку, видаляємо стилі для кнопок пресетів
 dateInputs.forEach(input => {
     input.addEventListener('change', () => {
         error.innerHTML = '';
@@ -35,11 +37,13 @@ dateInputs.forEach(input => {
     });
 });
 
+//при зміні значення інпута startDate встановлюємо атрибут min для інпута endDate
 startDate.addEventListener('change', () => {
     endDate.setAttribute('min', new Date(startDate.value).toISOString().split('T')[0]);
     document.getElementById("end").disabled = false;
 });
 
+//при зміні значення інпута endDate встановлюємо атрибут max для інпута startDate
 endDate.addEventListener('change', () => {
     startDate.setAttribute('max', new Date(endDate.value).toISOString().split('T')[0]);
 });
@@ -48,8 +52,11 @@ countBtn.addEventListener('click', count);
 
 function clickBtnHandler(btn, list = [], type) {
     btn.addEventListener('click', () => {
-        classRemover(list, 'focused');
-        btn.classList.add('focused');
+        //викликаємо ф-ю для масиву кнопок
+        classRemover(list, 'focused'); 
+        //додаємо клас для кнопки
+        btn.classList.add('focused'); 
+        //перезаписуємо змінні в залежності від отриманого типу
         switch (type) {
             case 'daysType':
                 daysType = document.querySelector('.days-type.focused').innerHTML;
@@ -65,12 +72,14 @@ function clickBtnHandler(btn, list = [], type) {
     });
 }
 
+//функція, яка видаляє клас для кожної кнопки з масиву
 function classRemover(list, className) {
     list.forEach(btn => {
         btn.classList.remove(className);
     });
 }
 
+//функція для отримання кількості днів з масиву дат в залежнності від обраної опції
 function filterType(startDateValue, endDateValue, type) {
     let countDay = 0;
     const dateList = createDateList(startDateValue, endDateValue);
@@ -100,9 +109,11 @@ function filterType(startDateValue, endDateValue, type) {
     return countDay;
 }
 
+//функція для отримання масиву дат в заданому проміжку
 function createDateList(startDateValue, endDateValue) {
     let currentDate = startDateValue;
     const dateList = [];
+    //тривалість дня в мілісекундах
     const day = 60 * 60 * 24 * 1000;
     while (currentDate.getTime() < endDateValue.getTime()) {
         dateList.push(currentDate);
@@ -111,14 +122,18 @@ function createDateList(startDateValue, endDateValue) {
     return dateList;
 }
 
+//функція, яка встановлює значення startDate і endDate інпутів в залежності від обраного пресету
 function setPreset() {
     const day = 60 * 60 * 24 * 1000;
+    //якщо інпут startDate має значення, то отримуємо його, якщо ні використовуємо поточну дату
     const startDateValue = startDate.value ? new Date(startDate.value) : new Date();
+    //getTime - повертає час у мілісекундах, new Date() - повертає об’єкт дати
     const week = new Date(startDateValue.getTime() + day * 7);
     const month = new Date(startDateValue.getTime() + day * 30);
     error.innerHTML = '';
     switch (presetType) {
         case 'Week':
+            //toISOString().split('T')[0] - отримуємо рядок у форматі ISO, сплітимо до букви Т, з масиву отримуємо 1 елемент
             startDate.value = startDateValue.toISOString().split('T')[0];
             endDate.value = week.toISOString().split('T')[0];
             break;
@@ -129,14 +144,18 @@ function setPreset() {
     }
 }
 
+//функція для підрахунку 
 function count() {
     dateValidation();
     if (!isValidated) {
         return;
     }
+    //отримання значень інпутів
     const startDateValue = new Date(startDate.value);
     const endDateValue = new Date(endDate.value);
+    //отримання кількості днів
     const countDay = filterType(startDateValue, endDateValue, daysType);
+    //калькуляція результату в залежності від обраної опції
     switch (measureType) {
         case 'Days':
             result.value = `${countDay} d`;
@@ -155,6 +174,7 @@ function count() {
     storeInHistory();
 }
 
+//функція для перевірки startDate і endDate інпутів після натискання на кнопку Count
 function dateValidation() {
     if (!startDate.value || !endDate.value) {
         error.innerHTML = `Dude, I don't see a date range!`;
@@ -164,20 +184,26 @@ function dateValidation() {
     isValidated = true;
 }
 
+//функція для запису в локалсторадж
 function storeInLocalStorage(startDateValue, endDateValue, result) {
     const formattedStartDate = startDateValue.toISOString().split('T')[0];
     const formattedEndDate = endDateValue.toISOString().split('T')[0];
     const results = JSON.parse(localStorage.getItem('results')) || [];
+    //якщо довжина масиву об’єктів досягає 10, перший елемент масиву видаляється
     if (results.length >= 10) {
         results.shift();
     }
+    //пушимо в масив об’єкт з ключами range і result
     results.push({ range: `${formattedStartDate} - ${formattedEndDate}`, result })
     localStorage.setItem('results', JSON.stringify(results));
 }
 
+//функція для відображення результатів, збережених в локалстораджі в таблиці history
 function storeInHistory() {
+    //отримуємо масив об’єктів з локалсторадж
     const results = JSON.parse(localStorage.getItem('results'));
     const table = document.querySelector('.history-data');
+    //якщо в локалстораджі не існує записів, таблиця не відображається
     if (!results) {
         table.style.display = 'none';
         return;
@@ -185,6 +211,7 @@ function storeInHistory() {
     const tableBody = table.querySelector('tbody')
     table.style.display = 'table';
     tableBody.innerHTML = null;
+    //промальовуємо таблицю, додаємо класи для стилізації, присвоюємо значення ключів з масиву локалстораджа рядкам таблиці
     results.forEach(resultItem => {
         const tableRow = document.createElement('tr');
         const tableCol1 = document.createElement('td');
